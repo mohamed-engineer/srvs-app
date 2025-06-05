d2bff10b2086cf594d76f9b111e4b3265387e2ce
Mohamed Hossam <plcacademy123@gmail.com>
2025-06-05 21:24:05 +0300

    Update prevworks.tsx

--- src/app/components/prevworks.tsx
+++ src/app/components/prevworks.tsx
@@ -97,7 +97,7 @@ export default function PrevWorksPage() {
 
   return (
     <>
-      <section id="prevworks" className="py-24 px-6 bg-gradient-to-br from-white via-gray-100 to-gray-900  text-gray-900 text-white min-h-screen">
+      <section id="prevworks" className="py-24 px-6 bg-gray-900  text-gray-900 text-white min-h-screen">
         <div className="max-w-6xl mx-auto">
           <h1 className="text-4xl font-extrabold text-center mb-12 tracking-wide">الأعمال السابقة</h1>
 
